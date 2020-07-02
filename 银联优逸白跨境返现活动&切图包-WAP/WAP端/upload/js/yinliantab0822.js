976f7262a1560cff9d35eb7936f74dab37ee0695
xyu <2660422481@qq.com>
2020-07-02 19:04:27 +0800

    添加 银联优逸白/白金卡跨境返现活动切图包

---
+++ 银联优逸白跨境返现活动&切图包-WAP/WAP端/upload/js/yinliantab0822.js
@@ -0,0 +1,106 @@
+$(function(){
+				$(".one").click(clickOne)
+
+		    function clickOne() {
+		        $(".top img:nth-child(1)").show();
+		        $(".top img:nth-child(2)").hide();
+		        $(".top img:nth-child(3)").hide();
+		        $(".top img:nth-child(4)").hide();
+		        $(".top img:nth-child(5)").hide();
+		        $(".top img:nth-child(6)").hide();
+		        
+		        $(".bottom div:nth-child(1)").show();
+		        $(".bottom div:nth-child(2)").hide();
+		        $(".bottom div:nth-child(3)").hide();
+		        $(".bottom div:nth-child(4)").hide();
+		        $(".bottom div:nth-child(5)").hide();
+		        $(".bottom div:nth-child(6)").hide();
+		    }
+		
+		    $(".two").click(clickTwo)
+		
+		    function clickTwo() {
+		       	$(".top img:nth-child(1)").hide();
+		        $(".top img:nth-child(2)").show();
+		        $(".top img:nth-child(3)").hide();
+		        $(".top img:nth-child(4)").hide();
+		        $(".top img:nth-child(5)").hide();
+		        $(".top img:nth-child(6)").hide();
+		        
+		       	$(".bottom div:nth-child(1)").hide();
+		        $(".bottom div:nth-child(2)").show();
+		        $(".bottom div:nth-child(3)").hide();
+		        $(".bottom div:nth-child(4)").hide();
+		        $(".bottom div:nth-child(5)").hide();
+		        $(".bottom div:nth-child(6)").hide();
+		    }
+		     $(".three").click(clickThree)
+		
+		    function clickThree() {
+		        $(".top img:nth-child(1)").hide();
+		        $(".top img:nth-child(2)").hide();
+		        $(".top img:nth-child(3)").show();
+		        $(".top img:nth-child(4)").hide();
+		        $(".top img:nth-child(5)").hide();
+		        $(".top img:nth-child(6)").hide();
+		        
+		        $(".bottom div:nth-child(1)").hide();
+		        $(".bottom div:nth-child(2)").hide();
+		        $(".bottom div:nth-child(3)").show();
+		        $(".bottom div:nth-child(4)").hide();
+		        $(".bottom div:nth-child(5)").hide();
+		        $(".bottom div:nth-child(6)").hide();
+		    }
+		
+		    $(".four").click(clickFour)
+		
+		    function clickFour() {
+		        $(".top img:nth-child(1)").hide();
+		        $(".top img:nth-child(2)").hide();
+		        $(".top img:nth-child(3)").hide();
+		        $(".top img:nth-child(4)").show();
+		        $(".top img:nth-child(5)").hide();
+		        $(".top img:nth-child(6)").hide();
+		        
+		       	$(".bottom div:nth-child(1)").hide();
+		        $(".bottom div:nth-child(2)").hide();
+		        $(".bottom div:nth-child(3)").hide();
+		        $(".bottom div:nth-child(4)").show();
+		        $(".bottom div:nth-child(5)").hide();
+		        $(".bottom div:nth-child(6)").hide();
+		    }
+		    $(".five").click(clickFive)
+		
+		    function clickFive() {
+		        $(".top img:nth-child(1)").hide();
+		        $(".top img:nth-child(2)").hide();
+		        $(".top img:nth-child(3)").hide();
+		        $(".top img:nth-child(4)").hide();
+		        $(".top img:nth-child(5)").show();
+		        $(".top img:nth-child(6)").hide();
+		        
+		       	$(".bottom div:nth-child(1)").hide();
+		        $(".bottom div:nth-child(2)").hide();
+		        $(".bottom div:nth-child(3)").hide();
+		        $(".bottom div:nth-child(4)").hide();
+		        $(".bottom div:nth-child(5)").show();
+		        $(".bottom div:nth-child(6)").hide();
+		    }
+		    $(".six").click(clickSix)
+		
+		    function clickSix() {
+		        $(".top img:nth-child(1)").hide();
+		        $(".top img:nth-child(2)").hide();
+		        $(".top img:nth-child(3)").hide();
+		        $(".top img:nth-child(4)").hide();
+		        $(".top img:nth-child(5)").hide();
+		        $(".top img:nth-child(6)").show();
+		        
+		       	$(".bottom div:nth-child(1)").hide();
+		        $(".bottom div:nth-child(2)").hide();
+		        $(".bottom div:nth-child(3)").hide();
+		        $(".bottom div:nth-child(4)").hide();
+		        $(".bottom div:nth-child(5)").hide();
+		        $(".bottom div:nth-child(6)").show();
+		    }
+			})
