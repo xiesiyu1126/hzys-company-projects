391217b7652768e55e23d7ceb6418a73659692d9
xyu <2660422481@qq.com>
2020-07-10 18:22:20 +0800

    添加双人自助餐&切图包 征信授权确认 东航电销修改

---
+++ 双人自助餐&切图包-WAP/upload/js/xize.js
@@ -0,0 +1,18 @@
+$(function () {
+    $("#page1").load("wsd-xize.html #content");
+    $("#page2").load("visa-xize.html #content");
+	$("#visa-img").hide();
+    $("#wsd").click(function () {
+        $("#page2").hide();
+        $("#page1").show();
+		$("#wsd-img").show();
+		$("#visa-img").hide();
+    })
+    $("#visa").click(function () {
+        $("#page1").hide();
+        $("#page2").show();
+        $("#wsd-img").hide();
+        $("#visa-img").show();
+    })
+
+})
