c82714f76b86b37f6e3b088447ae2a0b50a95e64
xyu <2660422481@qq.com>
2020-07-14 16:56:24 +0800

    好商贷确认版本 提交

---
+++ 好商贷&切图包-WAP/upload/js/index.js
@@ -0,0 +1,17 @@
+$(function () {
+    var linkAry = ["https://creditcardapp.bankcomm.com/loanmall/prod/carSerialList.html?carBrandId=267&brandName=领克",
+        "https://creditcardapp.bankcomm.com/loanmall/prod/carSerialList.html?carBrandId=28&brandName=悦达起亚",
+        "https://creditcardapp.bankcomm.com/loanmall/prod/carSerialList.html?carBrandId=49&brandName=雪弗兰",
+        "https://creditcardapp.bankcomm.com/loanmall/prod/carSerialList.html?carBrandId=34&brandName=吉利"
+    ];
+
+    let carAry = $(".car");
+    for (var i = 0; i < carAry.length; i++) {
+        carAry[i].index = i;
+        carAry[i].onclick = function () {
+            window.location.href = linkAry[this.index];
+
+        }
+    }
+
+})
