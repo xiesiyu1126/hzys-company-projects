bb6b5c5397934b6917ebb9784cdd2b3b00c6d2b3
xyu <2660422481@qq.com>
2020-07-23 19:18:48 +0800

    添加线上提款机切图包

---
+++ 线上取款机&切图包-WAP/upload/js/index.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $("#tanchuang").hide();
+
+    $("#close").click(function () {
+        $("#tanchuang").hide();
+    });
+    $("#jieshao").click(function () {
+        $("#tanchuang").show();
+    });
+
+})
