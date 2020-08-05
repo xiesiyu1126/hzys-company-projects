e3e9bd66e02ac51af6d8337d08b7a0e363c98a42
xyu <2660422481@qq.com>
2020-08-05 11:18:24 +0800

    修改项目名

--- 中铁卡&切图包-WeChat/check/js/back.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-	$(".btn-back").click(function(){
-		document.referrer === '' ? location.href = "https://creditcard.bankcomm.com/content/index.html" : window.history.back();return false;
-	});
-});
