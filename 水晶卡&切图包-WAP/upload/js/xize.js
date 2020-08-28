0171c1f4e70228292ec667e9bf2467f2780f1b1d
xyu <2660422481@qq.com>
2020-08-28 12:23:42 +0800

    水晶蜜卡-WAP js修改

--- 水晶卡&切图包-WAP/upload/js/xize.js
+++ 水晶卡&切图包-WAP/upload/js/xize.js
@@ -1,10 +1,11 @@
 $(function () {
-	$("#btn1").hide();
+    $("#btn1").hide();
+    $("#show2").hide();
     $("#change2").click(function () {
         $("#show1").hide();
         $("#show2").show();
-		$("#btn1").show();
-		$("#btn2").hide();
+        $("#btn1").show();
+        $("#btn2").hide();
     })
     $("#change1").click(function () {
         $("#show2").hide();
