2b0a1e1681c6b2d2ff3459b0bc2cd9de3f559c73
xyu <2660422481@qq.com>
2020-08-28 11:53:51 +0800

    PC 提交

---
+++ 水晶卡&切图包-PC/upload/js/xize.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("#btn1").hide();
+    $("#show2").hide();
+    $("#change2").click(function () {
+        $("#show1").hide();
+        $("#show2").show();
+		$("#btn1").show();
+		$("#btn2").hide();
+    })
+    $("#change1").click(function () {
+        $("#show2").hide();
+        $("#show1").show();
+        $("#btn1").hide();
+        $("#btn2").show();
+    })
+
+})
